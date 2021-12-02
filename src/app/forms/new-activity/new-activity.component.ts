f0ce374b4fb169b998d00fc817742f514fa1f418
Adrien <adrien.stievenart@live.be>
2021-12-02 14:44:42 +0100

    Agm-marker test

--- src/app/forms/new-activity/new-activity.component.ts
+++ src/app/forms/new-activity/new-activity.component.ts
@@ -9,6 +9,14 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 export class NewActivityComponent implements OnInit {
   latitude= 50.451714;
   longitude=3.985653;
+  latitude2= 50.437950;
+  longitude2=3.894009;
+  private mylocations = [
+    { lat: 7.423568, lng: 80.462287 },
+    { lat: 7.532321, lng: 81.021187 },
+    { lat: 6.117010, lng: 80.126269 }
+  ];
+
 
   form:FormGroup=this.fb.group({
     name:this.fb.control('',Validators.required),
@@ -36,5 +44,7 @@ export class NewActivityComponent implements OnInit {
   getPlace() {
     return this.controls['place']
   }
+
+
 
 }
